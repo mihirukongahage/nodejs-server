40fac3df50531a66d797d349b9d8e37b04b0e60b
mihirukongahage <mihirukongahage@gmail.com>
2022-05-29 23:32:50 +0530

    TX-29:undo functionality for notes deletion

--- app/server.js
+++ app/server.js
@@ -1,9 +1,11 @@
 const app = require("./index");
 const config = require("./config");
 
-app.listen(config.server.port, (err) => {
+var server = app.listen(config.server.port, (err) => {
   if (err) {
     log.error("Unable to start the server", error);
   }
   console.log("Listening to port 3000");
 });
+
+server.timeout = 5500;
