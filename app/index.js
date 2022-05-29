7c27b44aedb835b0486294c7710977eccbe0503d
mihirukongahage <mihirukongahage@gmail.com>
2022-05-29 21:44:08 +0530

    [TX-31]:Introduce a health endpoint

--- app/index.js
+++ app/index.js
@@ -14,6 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
